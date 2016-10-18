25d11ce0d21dd5d6688e3231be4dfe5bfde029c5
apache31 <apache3.1b@gmail.com>
2016-10-18 10:00:10 +0700

    Edit index.php add array2

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <h1>Index of PHP Basic2</h1>
         <?php
+        echo "<h2>Array 1</h2>";
         $array1=['a'=>25,'b'=>34,'c'=>27];
         
         print_r($array1);
@@ -20,6 +21,16 @@ and open the template in the editor.
         foreach ($array1 as $key => $value) {
             echo $key." : ".$value."<br>";
         }
+        
+        echo "<h2>Array 2</h2>";
+        $array2=["Java","Merida","S-Work","Infinite"];
+        
+        print_r($array2);
+        echo "<hr><h3>Foreach</h3>";
+        for($x=0;$x<count($array2);$x++) {
+            echo "array2[".$x."] : ".$array2[$x]."<br>";
+        }
+        
         ?>
     </body>
 </html>
